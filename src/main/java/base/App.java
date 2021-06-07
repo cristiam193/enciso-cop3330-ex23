1e25d6e1affe3707f2b27ade6d84543e095f7e94
cristiamenciso <cristiam.enciso@gmail.com>
2021-06-06 21:58:45 -0400

    Initial commit

---
+++ src/main/java/base/App.java
@@ -0,0 +1,111 @@
+package base;
+import java.util.Scanner;
+
+/*
+UCF COP3330 Summer 2021 Assignment 1 Solution
+Copyright 2021 Cristiam Enciso
+
+Exercise 23 - Troubleshooting Car Issues
+An expert system is a type of artificial intelligence program that uses a knowledge base and a set of rules to perform a task that a human expert might do. Many websites are available that will help you self-diagnose a medical issue by answering a series of questions. And many hardware and software companies offer online troubleshooting tools to help people solve simple technical issues before calling a human.
+
+Create a program that walks the user through troubleshooting issues with a car. Use the following decision tree to build the system:
+
+@startuml
+if (Is the car silent when you turn the key?) then (yes)
+    if (Are the battery terminals corroded?) then (yes)
+        :Clean terminals and try starting again.;
+        stop
+    else (no)
+        :Replace cables and try again.;
+        stop
+    endif
+else (no)
+    if (Does the car make a slicking noise?) then (yes)
+        :Replace the battery.;
+        stop
+    else (no)
+        if (Does the car crank up but fail to start?) then (yes)
+            :Check spark plug connections.;
+            stop
+        else (no)
+            if (Does the engine start and then die?) then (yes)
+                if (Does you car have fuel injection?) then (yes)
+                    :Get it in for service.;
+                    stop
+                else (no)
+                    :Check to ensure the choke is opening and closing.;
+                    stop
+                endif
+            else (no)
+                :This should not be possible.;
+                stop
+            endif
+        endif
+    endif
+endif
+@enduml
+Example Output
+Is the car silent when you turn the key? y
+Are the battery terminals corroded? n
+The battery cables may be damaged.
+Replace cables and try again.
+Constraint
+Ask only questions that are relevant to the situation and to the previous answers. Don’t ask for all inputs at once.
+
+Challenge
+Investigate rules engines and inference engines. These are powerful ways to solve complex problems that are based on rules and facts. Identify a rules engine for your programming language and use it to solve this problem
+ */
+
+public class App {
+    public static void main(String[] args) {
+        String yes = "y";
+        String no = "n";
+        Scanner in = new Scanner(System.in);
+        System.out.print("Is the car silent when you turn the key? ");
+        String answer1 = in.nextLine();
+        if(answer1.equals(yes) == true) {
+            System.out.print("Are the battery terminals corroded? ");
+            String answer2 = in.nextLine();
+            if(answer2.equals(yes) == true ){
+                System.out.println("Clean the terminals and try again");
+            }
+            else {
+                System.out.println("The battery may be damaged.\nReplace the cables and try again");
+            }
+
+        }
+        else {
+            System.out.print("Does the car make a slicking noise? ");
+            String answer3 = in.nextLine();
+            if(answer3.equals(yes) == true) {
+                System.out.println("Replace the battery");
+            }
+            else {
+                System.out.print("Does the car crank up but fail to start? ");
+                String answer4 = in.nextLine();
+                if(answer4.equals(yes) == true) {
+                    System.out.println("Check spark plug connections.");
+                }
+                else{
+                    System.out.print("Does the engine start and then die? ");
+                    String answer5 = in.nextLine();
+                    if(answer5.equals(yes) == true){
+                        System.out.print("Does your care have fuel injection? ");
+                        String answer6 = in.nextLine();
+                        if(answer6.equals(yes) == true){
+                            System.out.println("Get it in for service");
+                        }
+                        else{
+                            System.out.println("Check to insure the chock is opening and closing");
+                        }
+                    }
+                    else {
+                        System.out.println("This should be impossible");
+                    }
+                }
+            }
+
+        }
+
+    }
+}
